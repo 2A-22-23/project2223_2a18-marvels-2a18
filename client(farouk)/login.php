9743d58300cd77ac74ff0557fbbe5f65f45836ed
mohamed farouk chbichib <100930477+Kayamii@users.noreply.github.com>
2022-12-02 21:05:11 +0100

    login try 151dz456156161616

--- client(farouk)/login.php
+++ client(farouk)/login.php
@@ -1,27 +1,63 @@
+<?php
+session_start();
+include "connect.php";
+if(isset($_POST["login"])){
+if($_POST["email"]=="" or $_POST["mdp"]="")
+{echo "<center> <h1>veuillez remplire tout les champs</h1></center>";
+}else {
+$email=trim($_POST["email"]);
+$mdp=strip_tags(trim($_POST["mdp"]));
+$query=$db->prepare("SELECT * FROM login WHERE email=? AND mdp=?");
+$query->execute(array($email,$mdp));
+$control=$query->fetch(PDO::FETCH_OBJ);
+if ($control>0)
+{$_SESSION["email"]=$email;
+  header("Location:success.php");
+}
+echo "<center><h1>email ou mdp incorrect</h1></center>";
+
+}
+
+
+
+}
+
+?>
+
+
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script defer src="script.js"></script>
-    <link rel="stylesheet" href="style.css"> 
-    <title>Document</title>
-    <script> alert("fofofofofofofo");</script>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
 </head>
 <body>
-<div id="error"></div>
-<form id="inscription-form" action="/" method="GET">
-  <div>
-    <label for="name">Name</label>
-    <input id="name" name="name" type="text" >
-  </div>
-  <div>
-    <label for="password">Password</label>
-    <input id="password" name="password" type="password">
-  </div>
-  <button type="submit">Submit</button>
+ <div class="farouk">
+<form action="">
+  <p>
+<label for="">Email</label>
+<input name="email" type="email">
+</p>
+<p>
+<label for="">mdp</label>
+<input name="mdp" type="password">
+</p>
+ <button type="submit" name="login">Login</button> 
+
+
+
+
+
 </form>
 
+
+
+ </div>
 </body>
 </html>

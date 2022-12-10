c5ff20c645304927576e0105f8648cbaa63c4891
emnachihi <emna.chihi@esprit.tn>
2022-12-10 11:25:50 +0100

    img

--- cc/view/addVoiture.php
+++ cc/view/addVoiture.php
@@ -1,34 +1,12 @@
+
 <?php
 
 include '../controller/voitureC.php';
 
 $error = "";
-
 // create voiture
 $voitures = null;
-if (isset($_FILES['image']) && $_FILES['image']['error'] == 0)
-    {
-            // Testons si le fichier n'est pas trop gros
-            if ($_FILES['image']['size'] <= 8000000)
-            {
-                    // Testons si l'extension est autorisée
-                    $fileInfo = pathinfo($_FILES['image']['name']);
 
-                    $extension = $fileInfo['extension'];
-                    $allowedExtensions = ['jpg', 'jpeg', 'png'];
-
-                    $_FILES['image']['name']=$_POST["nom_voiture"].'.'.$extension;
-                   
-                    if (in_array($extension, $allowedExtensions))
-                    {
-                            // On peut valider le fichier et le stocker définitivement
-                            move_uploaded_file($_FILES['image']['tmp_name'], '../uploads/' . basename($_FILES['image']['name']));
-                            $url = basename($_FILES['image']['name']);
-            
-                    }
-            }
-            
-    }
 
 if (
     isset($_POST["idvoiture"])&&
@@ -53,7 +31,8 @@ if (
             $_POST['idvoiture'],
             $_POST['nom_voiture'],
             $_POST['idcategorie'],
-            $_POST['prix']
+            $_POST['prix'],
+            $url
            
         ); 
         // create an instance of the controller
@@ -64,41 +43,19 @@ $voitureC = new voitureC();
         $error = "Missing information";
 }
 
-
 ?>
+<!DOCTYPE html>
 <html lang="en">
-
 <head>
-<meta charset="utf-8">
-    <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>add</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="shortcut icon" type="image/png" href="../assets/images/icon/favicon.ico">
-    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
-    <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
-    <link rel="stylesheet" href="../assets/css/themify-icons.css">
-    <link rel="stylesheet" href="../assets/css/metisMenu.css">
-    <link rel="stylesheet" href="../assets/css/owl.carousel.min.css">
-    <link rel="stylesheet" href="../assets/css/slicknav.min.css">
-    <!-- amchart css -->
-    <link rel="stylesheet" href="https://www.amcharts.com/lib/3/plugins/export/export.css" type="text/css" media="all" />
-    <!-- others css -->
-    <link rel="stylesheet" href="../assets/css/typography.css">
-    <link rel="stylesheet" href="../assets/css/default-css.css">
-    <link rel="stylesheet" href="../assets/css/styles.css">
-    <link rel="stylesheet" href="../assets/css/responsive.css">
-    <link rel="stylesheet" type="text/css" href="dashboard.css">
-    <!-- modernizr css -->
-    <script src="assets/js/vendor/modernizr-2.8.3.min.js"></script>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="dashboard.css">
-    <title>Back with cars</title>
+    <title>Document</title>
 </head>
-
 <body>
-<div class="container">
+
+    <div class="container">
         <div class="navigation">
             <ul>
                 <li>
@@ -122,37 +79,25 @@ $voitureC = new voitureC();
                 <li>
                     <a href="dashboard2.php">
                         <span class="icon"><ion-icon name="book-outline"></ion-icon></span>
-                        <span class="title">Categories</span>
+                        <span class="title">categorie</span>
                     </a>
                 </li>
 
+                
                 <li>
-                    <a href="#">
-                        <span class="icon"><ion-icon name="book-outline"></ion-icon></span>
-                        <span class="title">emna</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <span class="icon"><ion-icon name="book-outline"></ion-icon></span>
-                        <span class="title">chihi</span>
-                    </a>
-                </li>
-             
-                <li>
-                    <a href="#">
+                    <a href="dashboard.php">
                         <span class="icon"><ion-icon name="help-circle-outline"></ion-icon></span>
                         <span class="title">Help</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="dashboard.php">
                         <span class="icon"><ion-icon name="settings-outline"></ion-icon></span>
                         <span class="title">Settings</span>
                     </a>
                 </li>
                 <li>
-                    <a href="#">
+                    <a href="dashboard.php">
                         <span class="icon"><ion-icon name="log-out-outline"></ion-icon></span>
                         <span class="title">Sing Out</span>
                     </a>
@@ -160,12 +105,109 @@ $voitureC = new voitureC();
                 
             </ul>
         </div>
-        <div>
-        
-    </div>
+        <div class=inscription-form>
+        <form action="" method="POST" enctype="multipart/form-data">
+        <table bordercolor=black border="1" align="center">
+        <tr>
+                <td bgcolor=#287bff>
+                    <label for="idvoiture" class="txt"> ID voiture :
+                    </label>
+                </td>
+               <td bgcolor=#287bff><input type="number" name="idvoiture" id="idvoiture" maxlength="30" required></td>
+            </tr>
+            <tr >
+            <tr >
+                <td bgcolor=#287bff>
+                    <label for="nom_voiture" class="txt"> nom_voiture :
+                    </label>
+                </td>
+                <td bgcolor=#287bff><input type="text"  name="nom_voiture" id="nom_voiture" maxlength="30" required></td>
+            </tr>
+            <tr >
+                <td bgcolor=#287bff>
+                    <label for="id_categorie" class="txt">id_categorie :
+                    </label>
+                </td>
+                <td bgcolor=#287bff><input type="text"  name="idcategorie" id="idcategorie" maxlength="30" required  ></td>
+            </tr>
+            <tr >
+                <td bgcolor=#287bff>
+                    <label for="prix" class="txt">prix :
+                    </label>
+                </td>
+                <td bgcolor=#287bff><input type="text"  name="prix" id="prix" maxlength="30" required  ></td>
+            </tr>
+            <tr>
+                <td>
+                    <label for="image">image: </label>
+                </td>
+                <td>
+                    <input type="file" id="image" name="image" value="importer une image"/>
+                </td>
+            </tr> 
+            <td></td>
+                <td>
+                    <input type="submit" value="Save">
+                </td>
+                
+            </tr>
+
+        </table>
+    </form>
+                    </div>
+                    <style>
+.inscription-form {
+    position: absolute;
+    top: 50%;
+    left: 55%;
+    transform: translate(-50%, -50%);
+    width: 400px;
+    height : 350px;
+    color: white;
+  }
+  .inscription-form h1 {
+    font-size: 15px;
+    text-align: center;
+    text-transform: uppercase;
+    color : white;
+    margin: 40px 0;
+    
+  }
+  .inscription-form p {
+    font-size: 20px;
+    margin: 20px 0;
+    
+   
+  }
+  .inscription-form input {
+      font-size: 16px;
+      padding: 15px 10px;
+      width: 70%;
+      border-radius: 5px ;
+      border-color:rgb(36, 13, 13);
+      outline: none;
+      color: black;
+    }
+    .inscription-form button {
+      font-size: 13px;
+      font-weight: bold;
+      margin: 20px 0;
+      padding: 10px 15px;
+      width:25% ;
+      border: 0;
+      border-radius: 5px;
+      background-color:red;
+      
+    }
+    .inscription-form button:hover {
+      color:red ;
+      background-color: black;
+     
+  
+    }
+    </style>                  
 
 
-                                
                           
 
 <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
@@ -193,64 +235,5 @@ $voitureC = new voitureC();
     list.forEach((item)=> 
     item.addEventListener('mouseover',activeLink));
 </script>
-    <a href="Listevoiture.php">Back to list </a>
-    <hr>
-
-    <div id="error">
-        <?php echo $error; ?>
-    </div>
-
-    <div class="container">
-            <div class="login-box ptb--100">
-                <form method ='POST'>
-                    
-                    <div class="login-form-head">
-                        <h4>Cars Addition :) </h4>
-                        <p>Add Cars Here !</p>
-                    </div>
-                    <div class="login-form-body">
-                        <div class="form-gp">
-                            
-                            <input  type="text" name="idvoiture" placeholder="entrer l'id de voiture" id="idvoiture" >
-                            <i class="ti-email"></i>
-                            <div class="text-danger"></div>
-                        </div>
-                        <div class="form-gp">
-                            
-                            <input  type="text" name="nom_voiture" placeholder="entrer le nom de voiture" id="nom_voiture" >
-                            <i class="ti-lock"></i>
-                            <div class="text-danger"></div>
-                        </div>
-                        <div class="form-gp">
-                            
-                            <input  type="text" name="idcategorie" placeholder="entrer l'id categorie correspontdente" id="idcategorie" >
-                            <i class="ti-email"></i>
-                            <div class="text-danger"></div>
-                        </div>
-                        
-                        <div class="form-gp">
-                            
-                            <input type="text" name="prix" placeholder="entrer le prix de voiture" id="prix" >
-                            <i class="ti-lock"></i>
-                            <div class="text-danger"></div>
-                        </div>
-                        
-                    
-                            <div class="col-6 text-right">
-                                <a href="#">Addition Done !</a>
-                            </div>
-                        </div>
-                        <div class="submit-btn-area">
-                            <button id="form_submit" type="submit">Submit <i class="ti-arrow-right"></i></button>
-                        </div>
-                        <div class="form-footer text-center mt-5">
-                            <p class="text-muted">Good Day <a href="register.html">BYE</a></p>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
- 
 </body>
-
 </html>

e5119b0cbf4ffa98c2f449a986d474071450965c
emnachihi <emna.chihi@esprit.tn>
2022-12-10 11:18:08 +0100

    C

--- cc/controller/categorieC.php
+++ cc/controller/categorieC.php
@@ -4,6 +4,9 @@ include '../model/categorie.php';
 
 class categorieC
 {
+
+
+   
     function tri($recherche)
     {
        
@@ -19,18 +22,7 @@ class categorieC
             die('Error: ' . $e->getMessage());
         }
     }
-    $sql ='SELECT * FROM categorie WHERE nom_categorie LIKE "%'.$recherche.'%" ';
-    $db = config::getConnexion();
-    try {
-        $query = $db->prepare($sql);
-        $query->execute();
 
-        $list = $query->fetchAll();
-        return $list;
-    } catch (Exception $e) {
-        die('Error: ' . $e->getMessage());
-    }
-}
 
 public function listcategorie()
     {
